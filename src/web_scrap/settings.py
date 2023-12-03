2e5f8eaabaadeba2178303ac608eb97b6d5ed217
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2023-12-03 01:22:57 -0300

    refactor parse_cpu callback and enable settings

--- src/web_scrap/settings.py
+++ src/web_scrap/settings.py
@@ -12,8 +12,6 @@ BOT_NAME = "web_scrap"
 SPIDER_MODULES = ["web_scrap.spiders"]
 NEWSPIDER_MODULE = "web_scrap.spiders"
 
-RETRY_HTTP_CODES = [429]
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "web_scrap (+http://www.yourdomain.com)"
 
@@ -26,7 +24,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+
+DOWNLOAD_DELAY = 10
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -51,10 +51,10 @@ DOWNLOAD_DELAY = 3
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DDOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
-    'flat.middlewares.TooManyRequestsRetryMiddleware': 543,
-}
+# DDOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+#     'flat.middlewares.TooManyRequestsRetryMiddleware': 543,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -70,7 +70,7 @@ DDOWNLOADER_MIDDLEWARES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
@@ -79,7 +79,7 @@ DDOWNLOADER_MIDDLEWARES = {
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -93,3 +93,33 @@ DDOWNLOADER_MIDDLEWARES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+
+################# PROXY #################
+
+# # Retry many times since proxies often fail
+# RETRY_TIMES = 10
+# # Retry on most error codes since proxies fail for different reasons
+# RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408, 429]
+
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+#     'scrapy_proxies.RandomProxy': 100,
+#     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+# }
+
+# # Proxy list containing entries like
+# # http://host1:port
+# # http://username:password@host2:port
+# # http://host3:port
+# # ...
+# PROXY_LIST = './web_scrap/proxies.txt'
+
+# # Proxy mode
+# # 0 = Every requests have different proxy
+# # 1 = Take only one proxy from the list and assign it to every requests
+# # 2 = Put a custom proxy to use in the settings
+# PROXY_MODE = 0
+
+# # If proxy mode is 2 uncomment this sentence :
+# #CUSTOM_PROXY = "http://host1:port"
--- src/web_scrap/spiders/cpus_spider.py
+++ src/web_scrap/spiders/cpus_spider.py
@@ -6,15 +6,19 @@ import scrapy
 
 class CPUSSpider(scrapy.Spider):
     name = "cpus"
-    start_urls = [ "https://www.techpowerup.com/cpu-specs/" ]
+    start_urls = ["https://www.techpowerup.com/cpu-specs/"]
+
+    keys = [ "Physical", "Processor", "Performance", "Architecture", "Core", "Cache" ]
+
+    def debug(self, response):
+        from scrapy.shell import inspect_response
+        inspect_response(response, self)
 
     def parse(self, response):
         page = response.body
 
         table = response.css('table.processors')
         rows = table.css('tr')
-
-        rows = [rows[2]]
 
         for row in rows:
             follow_link = row.css('a::attr(href)').get()
@@ -29,16 +33,21 @@ class CPUSSpider(scrapy.Spider):
         # print(table.get_rows()[3])
 
     def parse_cpu(self, response):
-        
+
+        # proxy check
+        if not response.css("a.page-header__logo"):
+            yield response.Request(url=response.url, dont_filter=True)
+
         def extract_with_css(query, selector=response):
             return selector.css(query).get(default="").strip()
-        
+
         def extract_nested_td(selector, td_index):
-            # return selector.css("tr")[td_index].css("td::text").get().strip()
-            # must return text of td, even if has anchor tag inside td
             return selector.css("tr")[td_index].css("td").xpath(".//text()").get().strip()
 
-        # Get <section> tag which contains 
+        def extract_nested_td_with_contains(selector, contains_txt):
+            return selector.css(f"tr:contains('{contains_txt}')").css("td").xpath(".//text()").get().strip()
+
+        # Get <section> tag which contains
         # nested <h1> tags with following texts:
         # - "Physical"
         # - "Processor"
@@ -47,91 +56,84 @@ class CPUSSpider(scrapy.Spider):
         # - "Core"
         # - "Cache"
 
-        physical = response.css('section:contains("Physical")')
-        processor = response.css('section:contains("Processor")')
-        performance = response.css('section:contains("Performance")')
-        core = response.css('section:contains("Core")')
-        cache = response.css('section:contains("Cache")')
-        architecture = response.css('section:contains("Architecture")')
+        processor = {}
+        processor['Name'] = extract_with_css("h1.cpuname::text")
 
-        # from scrapy.shell import inspect_response
-        # inspect_response(response, self)
+        for key in self.keys:
+            section = response.css(f'section:contains("{key}")')
+            sectionData = {}
 
-        t = {
-            "Name": extract_with_css("h1.cpuname::text"),
-            "Physical": {
-                "socket": extract_nested_td(physical, 0),
-                "foundry": extract_nested_td(physical, 1),
-                "process_size": extract_nested_td(physical, 2),
-                "transistors": extract_nested_td(physical, 3),
-                "die_size": extract_nested_td(physical, 4),
-                "io_process_size": extract_nested_td(physical, 5),
-                "io_die_size": extract_nested_td(physical, 6),
-                "package": extract_nested_td(physical, 7),
-                "t_case_max": extract_nested_td(physical, 8),
-            },
-            "Processor": {
-                "market": extract_nested_td(processor, 0),
-                "production_status": extract_nested_td(processor, 1),
-                "release_date": extract_nested_td(processor, 2),
-                "launch_price": extract_nested_td(processor, 3),
-                "part_no": extract_nested_td(processor, 4),
-                "bundled_cooler": extract_nested_td(processor, 5)
-            },
-            "Performance": {
-                "frequency": extract_nested_td(performance, 0),
-                "turbo_clock": extract_nested_td(performance, 1),
-                "base_clock": extract_nested_td(performance, 2),
-                "multiplier": extract_nested_td(performance, 3),
-                "multiplier_unlocked": extract_nested_td(performance, 4),
-                "tdp": extract_nested_td(performance, 5),
-                "fp32": extract_nested_td(performance, 6),
-            },
-            "Architecture": {
-                "codename": extract_nested_td(architecture, 0),
-                "generation": extract_nested_td(architecture, 1),
-                "memory_support": extract_nested_td(architecture, 2),
-                "rated_speed": extract_nested_td(architecture, 3),
-                "memory_bus": extract_nested_td(architecture, 4),
-                "ecc_memory": extract_nested_td(architecture, 5),
-                "pci_express": extract_nested_td(architecture, 6),
-                "chipsets": extract_nested_td(architecture, 7)
-            },
-            "Core": {
-                "no_cores": extract_nested_td(core, 0),
-                "no_threads": extract_nested_td(core, 1),
-                "smp_no_cpus": extract_nested_td(core, 2),
-                "integrated_graphics": extract_nested_td(core, 3),
-            },
-            "Cache": {
-                "L1": extract_nested_td(cache, 0),
-                "L2": extract_nested_td(cache, 1),
-                "L3": extract_nested_td(cache, 2)
-            },
-        }
+            for tr in section.css("tr"):
+                th = tr.css("th")
+                td = tr.css("td")
+                # print("===========tr===========")
+                # print(th.xpath(".//text()").get().strip(), td.xpath(".//text()").get().strip())
+                
+                if th and td:
+                    sectionData[th.xpath(".//text()").get().strip()] = td.xpath(".//text()").get().strip()
 
-        print(t)
+            processor[key] = sectionData
 
-        yield t
+        yield processor
 
+        # physical = response.css('section:contains("Physical")')
+        # processor = response.css('section:contains("Processor")')
+        # performance = response.css('section:contains("Performance")')
+        # core = response.css('section:contains("Core")')
+        # cache = response.css('section:contains("Cache")')
+        # architecture = response.css('section:contains("Architecture")')
 
-        
+        # self.debug(response)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # yield {
+        #     "Name": extract_with_css("h1.cpuname::text"),
+        #     "Physical": {
+        #         "socket": extract_nested_td(physical, 0),
+        #         "foundry": extract_nested_td(physical, 1),
+        #         "process_size": extract_nested_td(physical, 2),
+        #         "transistors": extract_nested_td(physical, 3),
+        #         "die_size": extract_nested_td(physical, 4),
+        #         "io_process_size": extract_nested_td(physical, 5),
+        #         "io_die_size": extract_nested_td(physical, 6),
+        #         "package": extract_nested_td(physical, 7),
+        #         "t_case_max": extract_nested_td(physical, 8),
+        #     },
+        #     "Processor": {
+        #         "market": extract_nested_td(processor, 0),
+        #         "production_status": extract_nested_td(processor, 1),
+        #         "release_date": extract_nested_td(processor, 2),
+        #         "launch_price": extract_nested_td(processor, 3),
+        #         "part_no": extract_nested_td(processor, 4),
+        #         "bundled_cooler": extract_nested_td(processor, 5)
+        #     },
+        #     "Performance": {
+        #         "frequency": extract_nested_td(performance, 0),
+        #         "turbo_clock": extract_nested_td(performance, 1),
+        #         "base_clock": extract_nested_td(performance, 2),
+        #         "multiplier": extract_nested_td(performance, 3),
+        #         "multiplier_unlocked": extract_nested_td(performance, 4),
+        #         "tdp": extract_nested_td(performance, 5),
+        #         "fp32": extract_nested_td(performance, 6),
+        #     },
+        #     "Architecture": {
+        #         "codename": extract_nested_td(architecture, 0),
+        #         "generation": extract_nested_td(architecture, 1),
+        #         "memory_support": extract_nested_td(architecture, 2),
+        #         "rated_speed": extract_nested_td(architecture, 3),
+        #         "memory_bus": extract_nested_td(architecture, 4),
+        #         "ecc_memory": extract_nested_td(architecture, 5),
+        #         "pci_express": extract_nested_td(architecture, 6),
+        #         "chipsets": extract_nested_td(architecture, 7)
+        #     },
+        #     "Core": {
+        #         "no_cores": extract_nested_td(core, 0),
+        #         "no_threads": extract_nested_td(core, 1),
+        #         "smp_no_cpus": extract_nested_td(core, 2),
+        #         "integrated_graphics": extract_nested_td(core, 3),
+        #     },
+        #     "Cache": {
+        #         "L1": extract_nested_td(cache, 0),
+        #         "L2": extract_nested_td(cache, 1),
+        #         "L3": extract_nested_td(cache, 2)
+        #     },
+        # }
